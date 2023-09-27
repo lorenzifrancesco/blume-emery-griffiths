dbfe3b9112985da5dbb361e96826aaee71ed74a6
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2023-09-27 12:38:59 +0200

    refactor

---
+++ plotting_utils.py
@@ -0,0 +1,118 @@
+import matplotlib.pyplot as plt
+import os.path
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.colors as mcolors
+import numpy as np
+from ranges import *
+# WARN: since Colab cannot find the LaTeX library, in the plot preparation phase
+# it is better to have the script in local and uncomment the following lines for
+# have LaTeX fonts
+#
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "Helvetica"
+})
+#
+# reproduce figures with the (T/j, x) plane and different K
+# bSomething means \beta * Something
+#
+# solve (3.10) always valid in the assumption M!=0
+
+def plot_sign(mat, name="lower.pdf"):
+  N = np.shape(mat)[0]
+  [tj_vec, x_vec] = get_arrays()
+  extent = [x_vec.min(), x_vec.max(), tj_vec.min(), tj_vec.max()]
+  mask = np.zeros((N, N))
+  mask[mat > 0.0] = 1
+  fig, ax = plt.subplots()
+  # plt.contour(mask, cmap='hot', interpolation='nearest', extent=extent, levels=200)
+  plt.imshow(np.flip(mask, axis=0), cmap='hot', interpolation='nearest', extent=extent)
+  plt.scatter(2/3, 1/3, c='red', marker='+')
+  plt.xlabel('x')
+  plt.ylabel('T/J')
+  skip = 20
+  plt.xticks(ticks=x_vec[::skip],  labels=[format(x, '.2f') for x in x_vec[::skip]])
+  plt.yticks(ticks=tj_vec[::skip], labels=[format(x, '.2f') for x in tj_vec[::skip]])
+  plt.colorbar()
+  plt.title(name[:-4])
+  if False:
+    plt.show()
+  print("saving plot", name, "...")
+  plt.savefig(name, format="pdf")
+  return 0
+
+def plot_heatmap(mat, name="lower.pdf", type="normal"):
+  N = np.shape(mat)[0]
+  if type=="normal":
+    [tj_vec, x_vec] = get_arrays()
+  else:
+    [tj_vec, x_vec] = get_arrays_order()
+  extent = [x_vec.min(), x_vec.max(), tj_vec.min(), tj_vec.max()]
+  fig, ax = plt.subplots()
+  mat = np.clip(mat, -1, 1)
+  plt.imshow(np.flip(mat, axis = 0), extent = extent, cmap='hot', interpolation='nearest', vmin=np.min(mat), vmax=np.max(mat))
+  plt.colorbar()
+  plt.contour(x_vec, tj_vec, mat, colors='cyan',  levels=[0])
+
+  plt.scatter(2/3, 1/3, c='mediumseagreen', marker='+', s=150)
+  plt.xlabel('x')
+  plt.ylabel('T/J')
+  skip = 20
+  plt.xticks(ticks=x_vec[::skip],  labels=[format(x, '.2f') for x in x_vec[::skip]])
+  plt.yticks(ticks=tj_vec[::skip], labels=[format(x, '.2f') for x in tj_vec[::skip]])
+  plt.title(name[:-4])
+  if False:
+    plt.show()
+  print("", name, "...")
+  plt.savefig(name, format="pdf")
+  return 0
+
+def plot_contour(mat, name="lower.pdf", type="normal"):
+  N = np.shape(mat)[0]
+  if type=="normal":
+    [tj_vec, x_vec] = get_arrays()
+  else:
+    [tj_vec, x_vec] = get_arrays_order()
+  extent = [x_vec.min(), x_vec.max(), tj_vec.min(), tj_vec.max()]
+  fig, ax = plt.subplots()
+  np.clip(mat, -10, 10)
+  plt.contour(x_vec, tj_vec, mat, levels=200)
+  plt.scatter(2/3, 1/3, c='red', marker='+')
+  plt.xlabel('x')
+  plt.ylabel('T/J')
+  skip = 20
+  plt.xticks(ticks=x_vec[::skip],  labels=[format(x, '.2f') for x in x_vec[::skip]])
+  plt.yticks(ticks=tj_vec[::skip], labels=[format(x, '.2f') for x in tj_vec[::skip]])
+  plt.colorbar()
+  plt.title(name[:-4])
+  if False:
+    plt.show()
+  print("saving plot", name, "...")
+  plt.savefig(name, format="pdf")
+  return 0
+
+def plot_surface(mat, name="surf.pdf", type="normal"):
+  N = np.shape(mat)[0]
+  if type=="normal":
+    [tj_vec, x_vec] = get_arrays()
+  else:
+    [tj_vec, x_vec] = get_arrays_order()
+  
+  X, Y = np.meshgrid(tj_vec, x_vec)
+  Z = mat
+  # Clip z values to be between -1 and 1
+  Z = np.clip(Z, -10, 10)
+  fig = plt.figure()
+  ax = fig.add_subplot(111, projection='3d')
+  surf = ax.plot_surface(X, Y, Z, cmap='viridis')
+  # Add color bar
+  fig.colorbar(surf)
+  # Set axis labels
+  ax.set_xlabel('X-axis')
+  ax.set_ylabel('Y-axis')
+  ax.set_zlabel('Z-axis')
+  # Set plot title
+  plt.title(name[:-4])
+  # Show the plot
+  plt.show()
+  return
---
+++ solver.py
@@ -0,0 +1,122 @@
+
+import numpy as np
+from scipy.optimize import root
+from tqdm import tqdm
+import threading
+
+
+def get_delta(bDelta, bK, x):
+  # FIX: there was also the -bK(1-x) term
+  return 1+1/2*np.exp(bDelta-bK)
+
+# betaA...
+def get_bA(d, tj):
+  return d/2-1/(2*tj) 
+
+def get_bB(d):
+  return 1/8*(d**2-d**3/3)
+
+def get_bC(d):
+  return 1/6*(1/2*d**3-3/8*d**4+3/40*d**5)
+
+def get_M_Delta(tj, x, bK, prev):
+  def eqs(pars):
+    M, Delta = pars
+    return [
+      1-x - 2*np.cosh(M/tj)/(np.exp(Delta/tj - bK*(1-x)) +2*np.cosh(M/tj)),
+      M - 2*np.sinh(M/tj)/(np.exp(Delta/tj - bK*(1-x)) +2*np.cosh(M/tj)),
+    ] 
+  initial_guesses = prev
+  root_solution = root(eqs, initial_guesses)
+  if not root_solution.success:
+    # print("WARNING: root finding failed!")
+    # print(root_solution.message)
+    pass
+  sol = root_solution.x
+  # print("Solved! results are M =", sol[0], "and Delta =", sol[1])
+  return sol
+
+
+def get_M_Delta_separate(tj, x, bK, prev):
+  def Meq(M):
+    return 1-x-M/np.tanh(M/tj)
+  initial_guesses = prev[0]
+  root_solution = root(Meq, initial_guesses)
+  if not root_solution.success:
+    # print("WARNING: root finding failed!")
+    # print(root_solution.message)
+    pass
+  M = root_solution.x
+  def Deltaeq(Delta):
+    return 1-x - 2*np.cosh(M/tj)/(np.exp(Delta/tj - bK*(1-x)) +2*np.cosh(M/tj))
+  # print("Solved! results are M =", sol[0], "and Delta =", sol[1])
+  initial_guesses = prev[1]
+  root_solution = root(Deltaeq, initial_guesses)
+  if not root_solution.success:
+    # print("WARNING: root finding failed!")
+    # print(root_solution.message)
+    pass
+  Delta = root_solution.x
+  return [M, Delta]
+
+def get_M_x(tj, Delta, bK, prev):
+  def eqs(pars):
+    M, x = pars
+    return [
+      1-x - 2*np.cosh(M/tj)/(np.exp(Delta/tj - bK*(1-x)) +2*np.cosh(M/tj)),
+      M - 2*np.sinh(M/tj)/(np.exp(Delta/tj - bK*(1-x)) +2*np.cosh(M/tj)),
+    ] 
+  initial_guesses = prev 
+  root_solution = root(eqs, initial_guesses)
+
+  if not root_solution.success:
+    print("WARNING: root finding failed!")
+    print(root_solution.message)
+  sol = root_solution.x
+  # print("Solved! results are M =", sol[0], "and Delta =", sol[1])
+  return sol
+
+def compute_orders(
+    N=200, 
+    K_over_J=0.0,
+    num_threads=1,
+    reset=True):  
+  print("\n============= defining vectors =============\n")
+  [tj_vec, Delta_vec] = get_arrays_order(N=N)
+  M      = np.ones((N, N))
+  x      = np.zeros((N, N))
+  suffix = '_' + K_over_J.__str__().replace(".", "_")
+  if not os.path.isfile('order_M'+suffix+'.npy') or (reset): # check only the first file
+    print("\n============= start computing =============\n")
+    # Define a worker function to fill the matrices
+    def worker(start, end):
+        for iD in tqdm(range(start, end)):
+          D = Delta_vec[iD]
+          prev = [1, 0.5]
+          for it, tj in enumerate(tj_vec):
+            bK = K_over_J * 1 / tj
+            if it == 0:
+               pluss = 0
+            start_point = [(2*M[it-1, iD]-M[it-1, iD]), x[it, iD-1]]
+            [M[it, iD], x[it, iD]] = get_M_x(tj, D, bK, prev = start_point)
+    chunk_size = len(Delta_vec) // num_threads
+    threads = []
+    for i in range(num_threads):
+        start = i * chunk_size
+        end = (i + 1) * chunk_size if i < num_threads - 1 else len(Delta_vec)
+        thread = threading.Thread(target=worker, args=(start, end))
+        threads.append(thread)
+        thread.start()
+    # Wait for all threads to finish
+    for thread in threads:
+        thread.join()
+    # Save the matrix to the file
+    print("\n============= saving =============\n")
+    np.save('order_M'+suffix+'.npy', M)
+    np.save('order_x'+suffix+'.npy', x)
+  else:
+    print("\n============= loading =============\n")
+    # Load the matrix from the file
+    M = np.load('order_M'+suffix+'.npy')
+    x = np.load('order_x'+suffix+'.npy')
+  return [M, x]
